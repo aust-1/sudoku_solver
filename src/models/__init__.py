a0ff41f33034a66ac37c218efba177781260ec95
austin <eliottroussille@gmail.com>
2025-08-09 04:42:21 +0200

    🚑 fix circular import

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,6 +1,6 @@
 """Model classes for Sudoku representation."""
 
-from models.board import Board
 from models.cell import Cell  # noqa: I001
+from models.board import Board
 
 __all__ = ["Board", "Cell"]
